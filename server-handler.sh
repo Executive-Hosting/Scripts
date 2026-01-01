4f9f84092794ecc80e4cbfccc5eb044cc122c2b7
Espryra <espryra@gmail.com>
2026-01-01 10:52:40 -0700

    Made server handler script

---
+++ server-handler.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Startup Warning
+echo "Server Handler Install Script"
+echo "Starting in 5 seconds..."
+sleep 5
+
+# Updating and Upgrading
+echo "Updating and Upgrading..."
+sleep 1
+apt update
+apt upgrade -y
+
+# Installing Requirements
+echo "Installing Requirements..."
+sleep 1
+apt install curl wget unzip git -y
+
+# Installing Bun
+echo "Installing bun..."
+sleep 1
+curl -fsSL https://bun.com/install | bash
+source /root/.bashrc
+
+# Installing Server Handler
+echo "Installing Server Handler..."
+sleep 1
+git clone https://github.com/Executive-Hosting/server-handler.git /root/server-handler/
+
+# Installing Server Handler Dependencies
+echo "Installing Server Handler Dependencies..."
+sleep 1
+cd /root/server-handler/
+bun install
+echo "DISCORDTOKEN=" >> /root/server-handler/.env
+echo "DISCORDID=" >> /root/server-handler/.env
+
+# Installing PM2
+echo "Installing PM2..."
+sleep 1
+bun install -g pm2
+echo "alias pm2=\"bunx pm2\"" >> /root/.bashrc
+source /root/.bashrc
+
+# Setting up PM2 Profile
+echo "Setting up PM2 Profile..."
+sleep 1
+echo "#!/bin/bash\n" >> /root/server-handler/start.sh
+echo "cd /root/server-handler/" >> /root/server-handler/start.sh
+echo "bun run src/main.ts" >> /root/server-handler/start.sh
+chmod +x /root/server-handler/start.sh
+
+
+echo "Server Handler Install Script Complete!"
+exit 0
